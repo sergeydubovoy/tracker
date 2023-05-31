78ee9cc651fa00a5222bc091ac68c89d64a8a901
Sergey Dubovoy <sergeydubovoi@gmail.com>
2023-06-01 00:55:34 +0300

    Added delegating events on checkboxes, delete btn

--- js/index.js
+++ js/index.js
@@ -22,20 +22,25 @@ const clearInput = () => {
 
 const renderTasksList = () => {
   const taskName = getTaskName();
-  tasks.push(taskName);
-  const taskTemplate = `
-  <div id="task" class="task">
-    <label for="checkbox" class="checkbox-cover">
-      <input type="checkbox" id="checkbox" class="task__checkbox"/>
-    </label>
-    <p id="task__name" class="task__name">${taskName}</p>
-    <button id="task__del-btn" class="task__del-btn">
-      <span id="del-btn__line" class="del-btn__line"></span>
-    </button>
-  </div>
-`;
-  tasksListNode.insertAdjacentHTML("afterbegin", taskTemplate);
-  clearInput();
+
+  if (!taskName) {
+    return;
+  } else {
+    tasks.push(taskName);
+    const taskTemplate = `
+    <div id="task" class="task">
+      <label for="checkbox" class="checkbox-cover">
+        <input type="checkbox" id="checkbox" class="task__checkbox"/>
+      </label>
+      <p id="task__name" class="task__name">${taskName}</p>
+      <button id="task__del-btn" class="task__del-btn">
+        <span id="del-btn__line" class="del-btn__line"></span>
+      </button>
+    </div>
+  `;
+    tasksListNode.insertAdjacentHTML("afterbegin", taskTemplate);
+    clearInput();
+  }
 };
 
 addTaskButtonNode.addEventListener("click", renderTasksList);
@@ -46,22 +51,35 @@ taskNameInputNode.addEventListener("keydown", (event) => {
   }
 });
 
-const toggleCheckboxes = () => {
-  taskNameNode.classList.toggle("task__name_checked");
-  deleteTaskButtonLineNode.classList.toggle("del-btn__line_checked");
-  taskNode.classList.toggle("task_checked");
+const toggleCheckboxes = (event) => {
+  if (event.target.classList.contains("task__checkbox")) {
+    const taskElement = event.target.closest(".task");
+
+    taskElement
+      .querySelector(".task__name")
+      .classList.toggle("task__name_checked");
+    taskElement
+      .querySelector(".del-btn__line")
+      .classList.toggle("del-btn__line_checked");
+    taskElement.classList.toggle("task_checked");
+  }
 };
 
-checkboxNode.addEventListener("click", toggleCheckboxes);
+tasksListNode.addEventListener("click", toggleCheckboxes);
 
-const deleteTask = () => {
-  const taskElement = taskNode;
-  const taskIndex =
-    tasksListNode.children.length -
-    Array.from(tasksListNode.children).indexOf(taskElement) -
-    1;
-  tasks.splice(taskIndex, 1);
-  taskElement.remove();
+const deleteTask = (event) => {
+  if (
+    event.target.classList.contains("task__del-btn") ||
+    event.target.classList.contains("del-btn__line")
+  ) {
+    const taskElement = event.target.closest(".task");
+    const taskIndex =
+      tasksListNode.children.length -
+      Array.from(tasksListNode.children).indexOf(taskElement) -
+      1;
+    tasks.splice(taskIndex, 1);
+    taskElement.remove();
+  }
 };
 
-deleteTaskButtonNode.addEventListener("click", deleteTask);
+tasksListNode.addEventListener("click", deleteTask);
